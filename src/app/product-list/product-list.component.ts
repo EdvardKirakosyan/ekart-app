a3d107782994a8934e16bb51c06881a00b04e4aa
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-11 15:47:06 +0400

    c

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,12 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  name = 'John Doe';
+
   product = {
     name: 'Iphone 13',
     color: 'red',
     price: 455,
     discount: 8.5,
     inStock: 5,
+    pImage: '/assets/images/iphone.png',
   };
 
   getDiscount() {
