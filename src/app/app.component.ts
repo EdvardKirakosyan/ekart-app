3c32f8084fdc45998d63342ebf16e9ac66eabc27
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-12 17:19:09 +0400

    с

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
