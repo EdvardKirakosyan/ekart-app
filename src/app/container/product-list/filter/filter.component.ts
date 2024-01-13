2805228b91b08fdaae7d2c0f8164ce9550b34fe7
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-13 12:51:24 +0400

    c

--- src/app/container/product-list/filter/filter.component.ts
+++ src/app/container/product-list/filter/filter.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
   imports: [],
   templateUrl: './filter.component.html',
-  styleUrl: './filter.component.css'
+  styleUrl: './filter.component.css',
 })
 export class FilterComponent {
+  @Input()
+  all: number = 0;
 
+  @Input()
+  inStock: number = 0;
+
+  @Input()
+  outOfstock: number = 0;
 }
