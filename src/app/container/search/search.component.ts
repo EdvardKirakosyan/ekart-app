35beca1dd35d554bd8bf1f8aab4326737e93d5b6
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-13 15:58:10 +0400

    c

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,25 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+import { Component, EventEmitter, Output } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css',
+  styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchText: string = '';
+  searchText: string = ''
 
   @Output()
   searchTextChange: EventEmitter<string> = new EventEmitter<string>()
 
-  onSearchTextChange() {
-    this.searchTextChange.emit(this.searchText)
-  }
+  onSearchTextChange() { }
 
-  updateSearchText(event: any) {
-    this.searchText = event.target.value;
+  updateSearchText(inputEl: HTMLInputElement) {
+    this.searchText = inputEl.value
+    this.searchTextChange.emit(this.searchText)
   }
 }
