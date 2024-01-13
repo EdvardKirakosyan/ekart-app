7f10012cc71a69e35e7c14566b2c637601228f5b
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-13 12:20:04 +0400

    c

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductComponent } from './product/product.component';
+import { FilterComponent } from './filter/filter.component';
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, FilterComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
