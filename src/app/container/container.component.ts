619f4569fec9d29659d9590c5fa7fdfe01b80d47
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-13 16:24:33 +0400

    c

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Component({
   selector: 'container',
   standalone: true,
-  imports: [SearchComponent, CommonModule, ProductListComponent],
+  imports: [SearchComponent, CommonModule, ProductListComponent, ProductDetailComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
